a2b02987a94fdc04a47dfa1e3aff32b843dd8b5e
Alan Pecenkovic <alanquad@yahoo.com>
2022-04-21 13:12:15 +0200

    Manually creating server component files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComopnent } from './server/server.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComopnent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
