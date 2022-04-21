b7cae32d09f1f5d6f50aef58db48da7303dd4f7f
Alan Pecenkovic <alanquad@yahoo.com>
2022-04-21 13:42:56 +0200

    Generating components with the CLI and nesting components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ServerComopnent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComopnent],
+  declarations: [AppComponent, ServerComopnent, ServersComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
