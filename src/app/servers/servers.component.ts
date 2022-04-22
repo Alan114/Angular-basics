6ca7a2b6bd7f3085940e7e2da1cdeca733bb10e0
Alan Pecenkovic <alanquad@yahoo.com>
2022-04-22 10:10:52 +0200

    Completed assignment from Max

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'Ne server was created!';
-  serverName = '';
+  serverName = 'Testserver';
+  userName = '';
 
   constructor() {
     setTimeout(() => {
@@ -19,10 +20,15 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {}
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  resetUser() {
+    this.userName = '';
   }
 }
