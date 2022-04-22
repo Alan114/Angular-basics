1a9c9db90e0ab84a9bf49b83a9c27662dbc8637a
Alan Pecenkovic <alanquad@yahoo.com>
2022-04-22 09:42:40 +0200

    Two-way databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComopnent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
@@ -8,8 +8,14 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComopnent, ServersComponent, SuccessAlertComponent, WarningAlertComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    ServerComopnent,
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
+  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
